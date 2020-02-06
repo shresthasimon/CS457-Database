80ddc1ccafb64df9c7110619a87e3101d50d132c
Simon <shresthasimon@yahoo.com>
2020-02-05 22:57:02 -0800

    setup

---
+++ database.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sshrestha on 2/5/20.
+//
+
+#include "database.h"
---
+++ database.h
@@ -0,0 +1,14 @@
+//
+// Created by sshrestha on 2/5/20.
+//
+
+#ifndef SIMONSHRESTHA_PA1_DATABASE_H
+#define SIMONSHRESTHA_PA1_DATABASE_H
+
+
+class database {
+
+};
+
+
+#endif //SIMONSHRESTHA_PA1_DATABASE_H
